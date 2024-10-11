6c0065557ab1622275816b20cae1cc3b6cb655d0
LocPhanFullstack <phananhloc03102001@gmail.com>
2024-10-11 23:02:19 +0700

    Add new controller and new routes

---
+++ src/controllers/userController.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(201).json(users);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${error.message}` });
+  }
+};
